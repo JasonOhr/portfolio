499c4ea5a3ff2c618eb1a664b8eb34d50c3ea607
Jason Ohr <jason.ohr@gmail.com>
2015-08-09 11:23:05 -0400

    Multiple additions 1. Feature - highlight menu bar based on page location 2. Added 45 deg slants using css background-img repeat-linear-gradient 3. Added some pics 5. Added transitions to menu

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,57 @@
 
   $(document).ready(function(){
     $('body').prepend(JST['application']());
+    // Cache selectors
+    var lastId,
+        topMenu = $("#top-menu"),
+        topMenuHeight = topMenu.outerHeight()+15,
+    // All list items from menu
+        menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+            //pulls associated id from anchors href from the menu
+            var inside = $(this).attr("href");
+            //now find 'inside's ' anchor and assign it to item
+            //this can be done in one step, tho I'm breaking it down for my own clarity
+            var item = $(inside);
+          if (item.length) { return item; }
+            //here, the href # has no length because it doesn't have a
+            //corresponding anchor accompany it
+        });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+    menuItems.click(function(e){
+      var href = $(this).attr("href"),
+          offsetTop = href === "#" ? 0 : $(href).offset().top;
+      $('html, body').stop().animate({
+        scrollTop: offsetTop
+      }, 700);
+      e.preventDefault();
+    });
+
+// Bind to scroll
+    $(window).scroll(function(){
+      var mid = $(window).height()/2;
+      // Get container scroll position
+      var fromTop = $(this).scrollTop()+mid;
+
+      // Get id of current scroll item
+      var cur = scrollItems.map(function(){
+        if ($(this).offset().top < fromTop)
+          return this;
+      });
+      // Get the id of the current element
+      cur = cur[cur.length-1];
+      var id = cur && cur.length ? cur[0].id : "";
+
+      if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href=#"+id+"]").parent().addClass("active");
+      }
+    });
   });
 })();
